3a82ae9bf47af72c8704671a21addfeef3437ab7
cychowai <71240864+cychowai@users.noreply.github.com>
2022-06-02 11:41:10 +0800

    add typing text, footer, style.css

---
+++ js/typingText.js
@@ -0,0 +1,8 @@
+const content = {
+  strings: ["programmer", "web developer", "coder", "HKUST graduate"],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 100,
+  backDelay: 1500,
+};
+let typingEffect = new Typed(".typingTextList", content);
