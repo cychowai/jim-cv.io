1579308c5ed24013ab806afec5a09c51b4e2f9c0
cychowai <71240864+cychowai@users.noreply.github.com>
2022-06-03 10:26:00 +0800

    Update typingText.js

--- js/typingText.js
+++ js/typingText.js
@@ -1,8 +1,8 @@
 const content = {
   strings: ["programmer", "web developer", "coder", "HKUST graduate"],
   loop: true,
-  typeSpeed: 50,
+  typeSpeed: 80,
   backSpeed: 100,
-  backDelay: 1500,
+  backDelay: 2000,
 };
 let typingEffect = new Typed(".typingTextList", content);
